c78712d42512e6b7158e93b2e248f6dc8ba2efae
Senuri Rupasinghe <senuri.rupasinghe@rutgers.edu>
2025-03-27 22:26:13 -0400

    app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 // frontend/src/App.jsx
-
+// had no time today to work on this-- just examined the code for today, will work extra hard tomorrow
 import React from "react";
 import QuantumGraph from "./components/QuantumGraph";
 
